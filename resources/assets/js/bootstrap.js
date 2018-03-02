8704f8486bd00d187c5134e5aafafbce49bcea6f
Bilal Maqsood <bilalsweet786@gmail.com>
2018-03-02 22:49:16 +0500

    Creating services and utils

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -31,8 +31,11 @@ Noty.overrideDefaults({
     timeout: 4000
 });
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios = axios;
+axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -9,7 +9,7 @@ export default new Router({
     routes: [
         // { path: '/', component: HomePage }, // doesn't exist yet!
         {
-            path: '/login',
+            path: '/admin/login',
             name: 'Login',
             component: Login
         }
